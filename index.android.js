9918c5b097a4a548ac7604fd313c0104df095844
Hugo Bragança da Silva <hugobraganca94@gmail.com>
2017-12-10 13:26:30 -0200

    componente resultado e lógica aplicados
     - criação do component resultado;
     - import do component resuldato ao indes.android.js;
     - aplicação da lógica do app.

--- index.android.js
+++ index.android.js
@@ -5,6 +5,7 @@ import { Router, Scene } from 'react-native-router-flux';
 import Principal from './src/components/Principal';
 import SobreJogo from './src/components/SobreJogo';
 import OutrosJogos from './src/components/OutrosJogos';
+import Resultado from './src/components/Resultado';
 
 export default class CaraOuCoroa extends Component {
   render() {
@@ -12,8 +13,9 @@ export default class CaraOuCoroa extends Component {
       <Router>
         <Scene>
           <Scene key='principal' component={Principal} initil title='Cara ou Coroa'/>
-          <Scene key='sobrejogo' component={SobreJogo} />
-          <Scene key='outrosjogos' component={OutrosJogos} />
+          <Scene key='sobrejogo' component={SobreJogo} title='Sobre' />
+          <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos'/>
+          <Scene key='resultado' component={Resultado} title='Resultado'/>
         </Scene>
       </Router>
     );
